154456031fb7f567270731b9099f08891de76500
khonager <khonager@users.noreply.github.com>
2026-01-11 22:41:22 +0000

    deploy: f2c75be3b5a7541cdb5ad86ce31ea8ce1fd595de

--- dev/flutter_service_worker.js
+++ dev/flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/lib/assets/logo.png": "fdf58309f364427d2a02f5653f8a181f",
 "assets/assets/icon.png": "c924385a5a13cdc1879648cdc1ffe8b1",
 "assets/assets/logo.png": "c924385a5a13cdc1879648cdc1ffe8b1",
-"assets/fonts/MaterialIcons-Regular.otf": "e144e4c4a3be0d404d1dd905aa39d7e3",
+"assets/fonts/MaterialIcons-Regular.otf": "19547eeee88db3c7f82ee7cc97806fd6",
 "assets/NOTICES": "ffc7b469fda1baf3b14c0027d7e1c76c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
@@ -40,9 +40,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "0f214bc6598dde7f52eff510d7d188f7",
 "404.html": "b62c68047e3ff4d26977811caf8ae5c5",
-"flutter_bootstrap.js": "807f0547c662968eeb2d788e6109b71f",
+"flutter_bootstrap.js": "f31e6f2ea59897bf1eaa7a73b5ec3645",
 "version.json": "fbeb6e4b64b41954352669a8c30ad340",
-"main.dart.js": "3681a44f7009fa9405c1db6c1df38cd6"};
+"main.dart.js": "fa1ae6ad0fba949c540f16d00f36edd8"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
