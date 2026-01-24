8584fcfb47bb9502aca25416263e10dfb3231bcb
khonager <khonager@users.noreply.github.com>
2026-01-24 05:45:05 +0000

    deploy: f9f8ebdafbc32d66b5df0cc2fe97197100ea94b6

--- dev/flutter_bootstrap.js
+++ dev/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "230477683"
+    serviceWorkerVersion: "865571554"
   }
 });
