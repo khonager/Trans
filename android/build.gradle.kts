a08f11006f0a1d9fa315049556da18506cf93c97
khonager <khonager@gmail.com>
2026-02-21 10:24:22 +0100

    fix android build action

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,16 +17,7 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
+// Build directory relocation removed to fix Flutter flavor compatibility and path monitoring issues
 subprojects {
     project.evaluationDependsOn(":app")
 
