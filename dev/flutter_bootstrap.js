55147ca6d69d00710608ca591754c33fddbc40bb
khonager <khonager@users.noreply.github.com>
2026-03-25 08:46:41 +0000

    deploy: 5e04cf8fa317a15ad1a94d7be0bfad18310bfe60

--- dev/flutter_bootstrap.js
+++ dev/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3109911504" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3282991770" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
