b6c6ae0ad71de9d1e3191497d501c1df0958bcfe
khonager <khonager@users.noreply.github.com>
2026-09-11 09:11:17 +0000

    deploy: 49135651eb9baac2c89735f692c25b0a5500729e

--- dev/main.dart.js
+++ dev/main.dart.js
@@ -57088,7 +57088,7 @@ $S:3}
 B.bJG.prototype={
 $1(a){var s=B.fT().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a804b261645ef8c13eb3d5c44a5c2fb0340c5539/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/06a2e2a110089dff50fe635cffd2a61e1b24fbcd/":s)+a},
 $S:29}
 B.ady.prototype={
 gt(a){var s=this.a
